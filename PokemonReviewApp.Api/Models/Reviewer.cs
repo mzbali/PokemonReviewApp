c4bbccfd50682ab56b1959f8ed8a8d9a36083547
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-04-29 14:53:30 +0100

    feat: update Owner, Review, and Reviewer with new properties

--- PokemonReviewApp.Api/Models/Reviewer.cs
+++ PokemonReviewApp.Api/Models/Reviewer.cs
@@ -3,6 +3,7 @@ namespace PokemonReviewApp.Api.Models;
 public class Reviewer
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public ICollection<Review> Reviews { get; set; }
 }
