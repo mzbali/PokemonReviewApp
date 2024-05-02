7452d124bbf1c4c4f8e7abeabdbfa86ff9a41335
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-05-02 14:39:36 +0100

    feat: add mappings for pokemon, review and reviewer DTOs

--- PokemonReviewApp.Api/Helper/MappingProfiles.cs
+++ PokemonReviewApp.Api/Helper/MappingProfiles.cs
@@ -9,6 +9,7 @@ public class MappingProfiles : Profile
     public MappingProfiles()
     {
         CreateMap<Pokemon, PokemonDto>();
+        CreateMap<PokemonDto, Pokemon>();
         CreateMap<Category, CategoryDto>();
         CreateMap<CategoryDto, Category>();
         CreateMap<Country, CountryDto>();
@@ -16,6 +17,8 @@ public class MappingProfiles : Profile
         CreateMap<Owner, OwnerDto>();
         CreateMap<OwnerDto, Owner>();
         CreateMap<Review, ReviewDto>();
+        CreateMap<ReviewDto, Review>();
         CreateMap<Reviewer, ReviewerDto>();
+        CreateMap<ReviewerDto, Reviewer>();
     }
 }
